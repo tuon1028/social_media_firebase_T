f6ae491869132f6a8e4f22cb70335054f7208546
tuon1028 <tuongcao24@gmail.com>
2023-12-21 22:44:09 +0700

    Init project

---
+++ Social-Media-Website-main/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuJytVyT9ZC7O5sMH3dA0N6DGGjd8gjgM",
+  authDomain: "aisc-9c269.firebaseapp.com",
+  projectId: "aisc-9c269",
+  storageBucket: "aisc-9c269.appspot.com",
+  messagingSenderId: "1066794399981",
+  appId: "1:1066794399981:web:b3eb60bff4d358834774e4",
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
